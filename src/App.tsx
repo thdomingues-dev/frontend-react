1a980ebdc0acfd49df3c37996bf12e9f729cf350
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-24 19:37:42 -0400

    Removido arquivos padrões do creat-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -20,18 +18,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, Stone!</h1>
         <p>{JSON.stringify(users)}</p>
       </header>
     </div>
