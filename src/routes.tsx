69a49f19bbfe7e755b3833fa811f15fa07940ea9
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-26 00:42:10 -0400

    Adicionado ao arquivo de rotas o caminho para usuários

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
+import Users from './pages/Users';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route path="/" component={LandingPage} exact />
+      <Route path="/users" component={Users} />
     </BrowserRouter>
   );
 }
