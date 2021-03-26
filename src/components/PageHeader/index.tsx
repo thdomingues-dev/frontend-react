53a1e37caac9640b25c1375117fb79b31574a044
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-26 01:21:09 -0400

    Implementado mecanismo de voltar para página anterior

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,13 +1,20 @@
+import { useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import './styles.css';
 
 const PageHeader = () => {
+  const history = useHistory();
+
+  function handleNavigateBack() {
+    history.goBack();
+  }
+
   return (
     <header className="page-header">
       <strong>GREENROCK</strong>
 
-      <div className="page-back-container">
+      <div className="page-back-container" onClick={handleNavigateBack}>
         <FiArrowLeft />
         Voltar
       </div>
