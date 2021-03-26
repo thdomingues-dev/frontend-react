1fd0bca307306a245e280f0bbcebe9e0ce5ec2f4
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-26 13:10:06 -0400

    Criada a estrutura do conteúdo da página de usuários

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -6,9 +6,24 @@ const Users = () => {
   return (
     <div className="users-container">
       <PageHeader />
-      <h1>
-        Users page!
-      </h1>
+      <h1>Usuários cadastrados</h1>
+
+      <p>Aqui é possível encontrar informações como: nome, documento, e-mail e muito mais.</p>
+
+      <div className="users-content">
+        <aside>
+          <div>Buscar users</div>
+        </aside>
+        <main>
+          <ul>
+            <li>Dev1</li>
+
+            <li>Dev2</li>
+
+            <li>Dev3</li>
+          </ul>
+        </main>
+      </div>
     </div>
   );
 }
