2703670feccec523280b51ef3b62dcb2b633be9a
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-26 00:54:27 -0400

    Adicionado ao arquivo de rotas o caminho para auditorias

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import Users from './pages/Users';
 import Cards from './pages/Cards';
+import Audits from './pages/Audits';
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route path="/" component={LandingPage} exact />
       <Route path="/users" component={Users} />
       <Route path="/cards" component={Cards} />
+      <Route path="/audits" component={Audits} />
     </BrowserRouter>
   );
 }
