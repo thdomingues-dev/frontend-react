2035efa6fe14cc67d838adccdbc6a020f80bef8b
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-26 00:47:08 -0400

    Adicionado ao arquivo de rotas o caminho para cartões

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import Users from './pages/Users';
+import Cards from './pages/Cards';
+
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route path="/" component={LandingPage} exact />
       <Route path="/users" component={Users} />
+      <Route path="/cards" component={Cards} />
     </BrowserRouter>
   );
 }
