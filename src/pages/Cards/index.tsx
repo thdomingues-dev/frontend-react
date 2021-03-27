fa53c6b73e4e30aec8d89f225891edd9929e78de
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-27 11:20:07 -0400

    Criada a grade de visualização dos cartões

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -8,6 +8,25 @@ const Cards = () => {
       <PageHeader />
       <h1>Consulta de cartões</h1>
       <p>Verifique status, valores e muito mais.</p>
+
+      <div className="cards-content">
+        <aside>Buscar cartão</aside>
+        <main>
+          <ul>
+            <li>
+              Card1Card1Card1
+            </li>
+
+            <li>
+              Card2Card2Card2
+            </li>
+
+            <li>
+              Card3Card3Card3
+            </li>
+          </ul>
+        </main>
+      </div>
     </div>
   );
 }
