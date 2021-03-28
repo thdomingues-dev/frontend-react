d5cded03d2343e3b6ec019458f4c061d039fd558
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-28 12:37:08 -0400

    feat(audits): Criado layout da página

--- src/pages/Audits/index.tsx
+++ src/pages/Audits/index.tsx
@@ -1,8 +1,53 @@
+import PageHeader from '../../components/PageHeader';
+import PageTitle from '../../components/PageTitle';
+
 import './styles.css';
 
 const Audits = () => {
   return (
-    <h1>Audits Page!</h1>
+    <div className="audits-container">
+      <PageHeader />
+      <PageTitle
+        title="Auditoria de logs"
+        description="Analise as operações realizadas no sistema."
+      />
+
+      <main>
+        <ul>
+          <li>
+            <strong>ID</strong>
+            <strong>Data/Hora</strong>
+            <strong>Antes</strong>
+            <strong>Depois</strong>
+            <strong>Analista</strong>
+          </li>
+
+          <li>
+            <p>0502</p>
+            <p>28/03/2021 00h00</p>
+            <p>Solicitado</p>
+            <p>Aprovado</p>
+            <p>Thales Domingues</p>
+          </li>
+
+          <li>
+            <p>0502</p>
+            <p>28/03/2021 00h00</p>
+            <p>Solicitado</p>
+            <p>Aprovado</p>
+            <p>Thales Domingues Domingues Domingues</p>
+          </li>
+
+          <li>
+            <p>202012</p>
+            <p>28/03/2021 00h00</p>
+            <p>Solicitado</p>
+            <p>Rejeitado</p>
+            <p>Lumena Dias da Fonseca Martins Rodrigues Alves da Silva</p>
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 }
 
