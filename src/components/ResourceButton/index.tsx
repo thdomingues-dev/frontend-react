3c990af501710bc3305460f915c794ede1835398
Thales Domingues <thdomingues.dev@gmail.com>
2021-04-02 23:25:17 -0400

    feat(landing): Ajustado layout da página

--- src/components/ResourceButton/index.tsx
+++ src/components/ResourceButton/index.tsx
@@ -1,22 +1,59 @@
 import { Link } from 'react-router-dom';
-import { FiChevronRight } from 'react-icons/fi';
+import { FiChevronRight, FiUsers, FiFileText } from 'react-icons/fi';
+import { AiOutlineIdcard } from 'react-icons/ai';
+import { FaRegCreditCard } from 'react-icons/fa';
 
 import './styles.css';
 
 interface ButtonProps {
   path: string;
   description: string;
+  icon?: string;
 }
 
-const ResourceButton = ({ path, description }: ButtonProps) => {
-  return (
-    <Link to={path}>
-      <span>
-        <FiChevronRight />
-      </span>
-      <p>{description}</p>
-    </Link>
-  );
+const ResourceButton = ({ path, description, icon }: ButtonProps) => {
+  switch (icon) {
+    case 'FiUsers':
+      return (<Link to={path}>
+        <span>
+          <FiUsers />
+        </span>
+        <p>{description}</p>
+      </Link>);
+
+    case 'FaRegCreditCard':
+      return (<Link to={path}>
+        <span>
+          <FaRegCreditCard />
+        </span>
+        <p>{description}</p>
+      </Link>);
+
+    case 'AiOutlineIdcard':
+      return (<Link to={path}>
+        <span>
+          <AiOutlineIdcard />
+        </span>
+        <p>{description}</p>
+      </Link>);
+
+    case 'FiFileText':
+      return (<Link to={path}>
+        <span>
+          <FiFileText />
+        </span>
+        <p>{description}</p>
+      </Link>);
+
+    default:
+      return (<Link to={path}>
+        <span>
+          <FiChevronRight />
+        </span>
+        <p>{description}</p>
+      </Link>);
+  }
+
 }
 
 export default ResourceButton;
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -19,40 +19,48 @@ const LandingPage = () => {
     <div className="page-container">
       <PageHeader />
 
-      <PageTitle
-        title={`Bem-vindo(a), ${handleNameFormated()}!`}
-        description="Escolha uma das opções abaixo para continuar."
-      />
-
-      <main>
-        <div className="group-container">
-          <h2>Recursos disponíveis</h2>
-
-          <div className="resource-group">
-            <ResourceButton
-              path="/users"
-              description=" Visualizar usuários"
-            />
-
-            <ResourceButton
-              path="/cards"
-              description=" Consultar cartões"
-            />
-
-            <ResourceButton
-              path="/newcard"
-              description=" Solicitar cartão"
-            />
-
-            {analyst.roles.includes("n2") &&
-              <ResourceButton
-                path="/audits"
-                description=" Obter auditoria"
-              />
-            }
-          </div>
+      <div className="page-container-grid">
+        <div className="page-title">
+          <PageTitle
+            title={`Bem-vindo(a), ${handleNameFormated()}!`}
+            description="Escolha uma das opções ao lado para continuar."
+          />
         </div>
-      </main>
+
+        <main>
+          <div className="group-container">
+            <h2>Recursos disponíveis</h2>
+
+            <div className="resource-group">
+              <ResourceButton
+                path="/users"
+                description=" Visualizar usuários"
+                icon="FiUsers"
+              />
+
+              <ResourceButton
+                path="/cards"
+                description=" Consultar cartões"
+                icon="FaRegCreditCard"
+              />
+
+              <ResourceButton
+                path="/newcard"
+                description=" Solicitar cartão"
+                icon="AiOutlineIdcard"
+              />
+
+              {analyst.roles.includes("n2") &&
+                <ResourceButton
+                  path="/audits"
+                  description=" Obter auditoria"
+                  icon="FiFileText"
+                />
+              }
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
