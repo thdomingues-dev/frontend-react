a858e022643e2cbe2e41b5cf8bef2e5e06466151
Thales Domingues <thdomingues.dev@gmail.com>
2021-04-06 20:10:57 -0400

    chore(deploy): Realizadas configurações entre heroku e github

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
-});
+const api = process.env.NODE_ENV === "production" ? axios.create({
+  baseURL: '/api',
+}) : (
+  axios.create({
+    baseURL: 'http://localhost:3001/api',
+  })
+);
 
 export default api;
