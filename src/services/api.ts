ce677468fc33c68c8cdcb2eb29822c624c4af70d
Thales Domingues <thdomingues.dev@gmail.com>
2021-10-05 23:51:32 -0300

    feat: improve api base-url logic

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const api = process.env.NODE_ENV === "production" ? axios.create({
-  baseURL: 'https://backend-tcc-solid.herokuapp.com/api',
-}) : (
-  axios.create({
-    baseURL: 'http://localhost:3001/api',
-  })
-);
+const api = axios.create({
+  baseURL: process.env.NODE_ENV === "production" 
+  ? 'https://backend-tcc-solid.herokuapp.com/api' 
+  : 'http://localhost:3001/api'
+});
 
 export default api;
